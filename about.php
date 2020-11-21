fb1389001d0b67704d281d49d21bf5df77ea8db6
sajag16642 <sajag16642@gmail.com>
2020-11-21 13:08:43 +0530

    first commit

---
+++ about.php
@@ -0,0 +1,69 @@
+<?php include 'includes/session.php'; ?>
+<?php include 'includes/header.php'; ?>
+<body class="hold-transition skin-blue layout-top-nav">
+<div class="wrapper">
+
+    <?php include 'includes/navbar.php'; ?>
+    <div class="content-wrapper">
+        <div class="container">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="bower_components/bootstrap/about.css" type="text/css">
+    <section class="team">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-10 col-md-offset-1">
+                    <div class="col-lg-12">
+                        <h6 class="description">OUR TEAM</h6>
+                        <div class="row pt-md">
+                            <div class="col-lg-3 col-md-3 col-sm-4 col-xs-12 profile">
+                                <div class="img-box">
+                                    <img src="images/sajag.jpg" class="img-responsive">
+                                    <ul class="text-center">
+                                        <a href="https://www.facebook.com/sajag.agrawal.71"><li><i class="fa fa-facebook"></i></li></a>
+                                        <a href="https://github.com/anon-007"><li><i class="fa fa-github"></i></li></a>
+                                    </ul>
+                                </div>
+                                <h1>Sajag Agrawal</h1>
+                                <h2>16642</h2>
+                                <p>Information Technology</p>
+                            </div>
+                            <div class="col-lg-3 col-md-3 col-sm-4 col-xs-12 profile">
+                                <div class="img-box">
+                                    <img src="images/saurabh.jpg" class="img-responsive">
+                                    <ul class="text-center">
+                                        <a href="https://www.facebook.com/saurabh.mark.37"><li><i class="fa fa-facebook"></i></li></a>
+                                        <a href="https://github.com/cap7nsaurabh/"><li><i class="fa fa-github"></i></li></a>
+                                    </ul>
+                                </div>
+                                <h1>Saurabh Singh</h1>
+                                <h2>16644</h2>
+                                <p>Information Technology</p>
+                            </div>
+                            <div class="col-lg-3 col-md-3 col-sm-4 col-xs-12 profile">
+                                <div class="img-box">
+                                    <img src="images/shailesh.jpg" class="img-responsive">
+                                    <ul class="text-center">
+                                        <a href="#"><li><i class="fa fa-facebook"></i></li></a>
+                                        <a href="#"><li><i class="fa fa-github"></i></li></a>
+                                    </ul>
+                                </div>
+                                <h1>Shailesh</h1>
+                                <h2>16645</h2>
+                                <p>Information Technolgy</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+        </div>
+    </div>
+
+    <?php include 'includes/footer.php'; ?>
+</div>
+
+<?php include 'includes/scripts.php'; ?>
+</body>
+</html>
---
+++ includes/footer.php
@@ -0,0 +1,11 @@
+<footer class="main-footer">
+    <div class="container">
+      <div class="pull-right hidden-xs">
+          <strong>Copyright &copy; 2018 <a href="about.php">About US</a></strong>
+
+      </div>
+        <div class="text-centre">
+            <b>All rights reserved</b>
+        </div>
+    </div>
+</footer>
